c87824019630b19f76a2a7a51c38f9cee2ad0f97
Paul <pbuol@msn.com>
2020-04-14 02:45:33 -0700

    debug update

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gdtpi9=d0-@c+=zip8*=+ti0o(9rclwt$x_awfz%rq*z05gt!#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
